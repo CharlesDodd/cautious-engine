73fbc554e298aa43ae971289ac8ffe42026797bb
Charles Dodd <54581344+CharlesDodd@users.noreply.github.com>
2020-07-20 17:34:54 +0100

    Update crypt_app.py

--- crypt_app/crypt_app.py
+++ crypt_app/crypt_app.py
@@ -26,9 +26,7 @@ class App(tk.Tk):
 
         for F in (StartPage, PageTwo):
             frame = F(container, self)
-
             self.frames[F] = frame
-
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
